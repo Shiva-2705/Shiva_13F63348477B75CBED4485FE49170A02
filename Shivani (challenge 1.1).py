917027850a13f824c100155f94a8be790275f75b
Shiva-2705 <144457853+Shiva-2705@users.noreply.github.com>
2023-09-08 19:11:49 +0530

    Add files via upload

---
+++ Shivani (challenge 1.1).py
@@ -0,0 +1,16 @@
+#recursive function to calculate the factorial of the number
+
+def fact_rec(n):
+  if (n==0 or n==1):
+    return 1
+  else:
+    return n*fact_rec(n-1)
+   
+num=int(input("Enter the number: "))
+res= fact_rec(num)
+print("The factorial of {} is {}". format(num, res))
+
+
+
+
+
